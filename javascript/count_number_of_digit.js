a5e2c3a844e65375c784e0aeac46ce7bc5b00cb2
LAVESH KUMBHALKAR <lavesh.kumbhalkar@gmail.com>
2025-07-03 22:22:16 +0530

    COUNT_OF_DIGIT

--- javascript/count_number_of_digit.js
+++ javascript/count_number_of_digit.js
@@ -8,5 +8,5 @@ function count_of_digit(n) {
     }
     return "Count of  digit [" + n + "] : " + count;
 }
-
+ 
 console.log(count_of_digit(-1234)); // Count of  digit [-1234] : 4
--- php/count_number_of_digit.php
+++ php/count_number_of_digit.php
@@ -11,6 +11,6 @@ function count_number_of_digit($number)
     }
     echo "Count of digit [{$orgNumber}] : " . "{$count}" . PHP_EOL;
 }
-
+ 
 count_number_of_digit(-150); // Count of digit [-150] : 3
 
